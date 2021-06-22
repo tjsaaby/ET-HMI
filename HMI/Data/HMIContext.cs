6ee61afb00e0d2986fe157c0e38012e2cb8eea7b
Thomas <Thomas.joon.nielsen@consultant.lego.com>
2021-06-22 22:52:45 +0200

    Adding controllers, DTOS, profiles

---
+++ HMI/Data/HMIContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HMI.Models;
+
+namespace HMI.Data
+{
+    public class HMIContext : DbContext
+    {
+        public HMIContext (DbContextOptions<HMIContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Sheet>()
+                .Property(s => s.SheetID)
+                .ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<Names>()
+                .Property(n=>n.NameID)
+                .ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<Sheet>()
+            .HasMany(s => s.Names)
+            .WithOne(n => n.Sheet)
+            .IsRequired();
+            //.OnDelete(DeleteBehavior.SetNull); // sets the foreign key value of the dependent entity to null in the event that the principal is deleted:
+        }
+
+        public DbSet<HMI.Models.Names> Names { get; set; }
+
+        public DbSet<HMI.Models.Sheet> Sheet { get; set; }
+
+       
+
+    }
+}
---
+++ HMI/Models/Names.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HMI.Models
+{
+    public class Names
+    {
+        [Key]
+        public string NameID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public string? SheetID { get; set; }
+        public virtual Sheet Sheet { get; set; }
+    }
+}
